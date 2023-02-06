cae1d2afddfdf2298d7cd5e7c2aa168770f12816
Maisin Edouard <92255720+YiddishGun11@users.noreply.github.com>
2023-02-06 15:56:16 +0100

    [UPDATE] Navbar first look

--- WebApp/Client/src/components/NavBar/NavBar.jsx
+++ WebApp/Client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,50 @@
 import "./NavBar.scss"
 
+//dependencies
+import {Link} from "react-router-dom"
+
+//svg's
+import {ReactComponent as DownArrow} from "../../assets/icons/down-icon.svg";
+import {ReactComponent as LangIcon} from "../../assets/icons/language-icon.svg";
+
 function NavBar(){
     return(
-        <div>
-            <h1>Nav</h1>
+        <div className="navbar-main-div">
+            <div className="navbar-logo">
+                <Link to="/website">
+                    <h1>Academix</h1>
+                </Link>
+            </div>
+            <div className="navbar-links">
+                <div className="navbar-lang">
+                    <LangIcon id="nav-lang-icon" />
+                    <DownArrow id="nav-arrow-icon"/>
+                    <LangDropdown />
+                </div>
+                <div className="vertical-line"></div>
+                <div>
+                    <p>toggle</p>
+                </div>
+                <div className="vertical-line"></div>
+                <div className="nav-login-link">
+                    <Link to="/website/login">Login</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+const LangDropdown = () => {
+    return(
+        <div className="navbar-dropdown">
+            <div className="selected-lang">
+                <p>English</p>
+            </div>
+            <div className="other-languages">
+                <p>French</p>
+                <p>Spanish</p>
+            </div>
         </div>
     )
 }
