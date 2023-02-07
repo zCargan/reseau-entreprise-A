6fa63f087f0d2afdaf22d2d2692371d5d4c8f679
CardinPatson <cardin.tiako@gmail.com>
2023-02-07 16:52:53 +0100

    feat(search): search page

--- WebApp/Client/src/pages/UserApp/Search/Search.jsx
+++ WebApp/Client/src/pages/UserApp/Search/Search.jsx
@@ -1,10 +1,11 @@
 import "./Search.scss";
 import NavBar from "../../../components/NavBar/NavBar";
 import Formation from "../../../components/Formation/formation";
+import SearchIcon from "@/assets/icons/search-icon.svg";
 import { ReactComponent as ListIcon } from "../../../assets/icons/list-icon.svg";
 function Search() {
 	// Contenue de l'affichage d'une formation
-	
+
 	return (
 		<>
 			<NavBar />
@@ -12,42 +13,17 @@ function Search() {
 				<div className="search__content">
 					<div className="search__theme">
 						<div className="search__theme__title">
-							Quelle domaine vous passionne ?
+							Quelle domaine rechercher vous ?
 						</div>
 						<div className="search__theme__body">
-							{/*TODO Faire une boucle avec tous les themes récupérés en backend  */}
-							{/* Lorsqu'on appuie sur un thème on filtre tous les thèmes */}
-							<button className="search__theme__button">
-								Developpement
-							</button>
-							<button className="search__theme__button">
-								Réseau
-							</button>
-							<button className="search__theme__button">
-								Electronique
-							</button>
-							<button className="search__theme__button">
-								Adminstration
-							</button>
-							<button className="search__theme__button">
-								AI
-							</button>
-							<button className="search__theme__button">
-								Electricité
-							</button>
-							<button className="search__theme__button">
-								Mobile
-							</button>
+							{/* Barre de recherche */}
+							{/* Barre de recherche avec un input */}
+							<img src={SearchIcon} alt="search" />
+							<input type="text" />
 						</div>
 					</div>
 					<div className="search__formation">
-						{/*TODO Faire une boucle avec tous les formations récupérés en backend */}
-						<Formation />
-						<Formation />
-						<Formation />
-						<Formation />
-						<Formation />
-						<Formation />
+						{/*TODO Faire une boucle avec tous les formations récupérés en backend qu'il souhaite rechercher */}
 					</div>
 				</div>
 			</div>
--- WebApp/Client/src/pages/Website/WebsiteLayout.jsx
+++ WebApp/Client/src/pages/Website/WebsiteLayout.jsx
@@ -1,7 +1,6 @@
 import {Outlet} from "react-router-dom";
 import { Suspense } from "react";
 
-import Navbar from "../../components/NavBar/NavBar";
 import FooterBar from "../../components/FooterBar/FooterBar";
 
 function WebsiteLayout(){
