c14127e2a11f037d7ae3716b8dd92b490e76a65b
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2023-02-07 09:47:49 +0100

    [bouse] No comment

--- WebApp/Client/src/components/SideBar/SideBar.jsx
+++ WebApp/Client/src/components/SideBar/SideBar.jsx
@@ -1,11 +1,15 @@
-import "./SideBar.scss"
+import "./SideBar.scss";
+import Logo from "@/assets/logo/logo.svg";
 
-function SideBar(){
-    return(
-        <div>
-            <h1>SideBar</h1>
-        </div>
-    )
+function SideBar() {
+	return (
+		<div className="side_bar">
+			<div className="logo_title">
+				<img src={Logo} className="logo" />
+				<h1 className="logo_text">Academox</h1>
+			</div>
+		</div>
+	);
 }
 
 export default SideBar;
--- WebApp/Client/vite.config.js
+++ WebApp/Client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { fileURLToPath, URL } from "node:url";
 import * as path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: { host: "0.0.0.0" },
