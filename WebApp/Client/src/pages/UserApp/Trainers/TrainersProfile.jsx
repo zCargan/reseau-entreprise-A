852dceba965790358d3da3409078f0dd2521fa78
HaAymar <he201905@students.ephec.be>
2023-02-09 13:42:25 +0100

    feat(update): update of the page trainers

--- WebApp/Client/src/pages/UserApp/Trainers/TrainersProfile.jsx
+++ WebApp/Client/src/pages/UserApp/Trainers/TrainersProfile.jsx
@@ -1,12 +1,15 @@
 import "./Trainers.scss";
 
-import Profile from "@/assets/image/bebe.jpg";
+import P1 from "@/assets/image/p1.avif";
+import P2 from "@/assets/image/p2.jpeg";
+import P3 from "@/assets/image/p3.webp";
+import P4 from "@/assets/image/p1.avif";
 
 function TrainersProfile() {
 	return (
 		<div className="container-trainer">
 			<div className="profile-person">
-				<img src={Profile} alt="photoProfile" className="profile_pic" />
+				<img src={P2} alt="photoProfile" className="profile_pic" />
 				<div className="profile-name">
 					<div className="name-p">Nom:</div>
 					<div className="name-m">Mazouille</div>
