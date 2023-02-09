143093d7d184880b8ab6dddf2906dc055aa8c876
CardinPatson <cardin.tiako@gmail.com>
2023-02-09 15:38:24 +0100

    add redux persist and fix reducer

--- WebApp/Client/src/main.jsx
+++ WebApp/Client/src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 import { Provider } from "react-redux";
-import store from "./store";
+import { persistor, store } from "./store";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</BrowserRouter>
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</PersistGate>
+	</Provider>
 );
--- WebApp/Client/src/store/index.jsx
+++ WebApp/Client/src/store/index.jsx
@@ -1,10 +1,50 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import localStorage from "localStorage";
 import rootReducer from "../reducer";
+import logger from "redux-logger";
 
-const store = configureStore({ reducer: rootReducer });
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import localForage from "localforage";
 
-export default store;
+const persistConfig = {
+	key: "root",
+	version: 1,
+	storage: localForage,
+	blacklist: [], //on met les reducers dont on ne veut pas sauver l'état (whitelist pour le contraire)
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [
+					FLUSH,
+					REHYDRATE,
+					PAUSE,
+					PERSIST,
+					PURGE,
+					REGISTER,
+				],
+			},
+		}),
+	// middleware: [thunk, logger],
+});
+
+export const persistor = persistStore(store);
+
+// export default store;
 
 // const persitConfig = {
 // 	key: "root",
